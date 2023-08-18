da0c8cf49dea242bb1011c4b6004f2c76b9478cd
kimdaisyyr <kimdaisyyr@gmail.com>
2023-08-18 03:37:17 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,20 +140,22 @@ for (let i = 0; i < formInputs.length; i++) {
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
+pages.addEventListener("click", function () { pages.classList.add("  active"); });
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("  active");
-        navigationLinks[i].classList.add("  active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("  active");
-        navigationLinks[i].classList.remove("  active");
-      }
-    }
-
-  });
-}
+// // add event to all nav link
+// for (let i = 0; i < navigationLinks.length; i++) {
+//   navigationLinks[i].addEventListener("click", function () {
+//
+//     for (let i = 0; i < pages.length; i++) {
+//       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+//         pages[i].classList.add("  active");
+//         navigationLinks[i].classList.add("  active");
+//         window.scrollTo(0, 0);
+//       } else {
+//         pages[i].classList.remove("  active");
+//         navigationLinks[i].classList.remove("  active");
+//       }
+//     }
+//
+//   });
+// }
