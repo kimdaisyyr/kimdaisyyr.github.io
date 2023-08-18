1f2a12d9349da546eca76ba84ce9cd518a544cb5
kimdaisyyr <kimdaisyyr@gmail.com>
2023-08-18 03:04:11 -0400

    Nav Bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,7 +136,7 @@ for (let i = 0; i < formInputs.length; i++) {
 
 
 // page navigation variables
-const navigationLinks = document.querySelectorAll("[data-navbar-link]");
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
 // add event to all nav link
